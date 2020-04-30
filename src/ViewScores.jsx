9e6a0c5a475a8915618ff568d36dc00060ac64c0
allezz <aerbay@gmail.com>
2020-05-01 01:09:21 +0200

    Add ViewScores page,Add routing in index

---
+++ src/ViewScores.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function ViewScores() {
+  return (
+    <div>
+      <p>Hello World</p>
+    </div>
+  );
+}
--- src/index.js
+++ src/index.js
@@ -1,11 +1,23 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import ViewScores from "./ViewScores";
+import { Switch, Route, BrowserRouter } from "react-router-dom";
+
+const AppPack = () => {
+  return (
+    <>
+      <Switch>
+        <Route exact path="/" component={App}></Route>
+        <Route exact path="/viewscores" component={ViewScores}></Route>
+      </Switch>
+    </>
+  );
+};
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  <BrowserRouter>
+    <AppPack />
+  </BrowserRouter>,
+  document.getElementById("root")
 );
-
